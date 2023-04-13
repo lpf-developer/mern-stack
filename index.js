2b4b61f2433571699ddaf9f55c0882edc95e81e7
Luis Paulo França <franca.luiz.paulo.777@gmail.com>
2023-04-13 10:28:42 -0300

    MERN-STACK - Aula 2:  falando um pouco sobre rotas

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("Deus seja louvado!")
+    const soma = 100 + 1
+    res.send({soma: soma})
+    // Outra forma: res.json(soma)
 })
 
 app.listen(3000)
